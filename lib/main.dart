0463d62f90a1fd37f514f5b07c4bfa338cebdd8f
Augustin <augustin@mycompanyfiles.fr>
2020-04-02 12:26:43 +0200

    Update Home page ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+// import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 
 import 'data/db/database.dart';
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,6 +12,20 @@ class _HomePageState extends State<HomePage> {
   DateTime now;
   bool showColon;
 
+  static const tStyle = TextStyle(
+    fontSize: 64,
+    fontStyle: FontStyle.italic,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+    shadows: <Shadow>[
+      Shadow(
+        offset: Offset(1.0, 1.0),
+        blurRadius: 2.0,
+        color: Colors.black,
+      ),
+    ],
+  );
+
   @override
   void initState() {
     now = DateTime.now();
@@ -39,28 +53,82 @@ class _HomePageState extends State<HomePage> {
           gradient: LinearGradient(
             begin: Alignment.topRight,
             end: Alignment.bottomLeft,
-            colors: [Colors.blue, Colors.red],
+            colors: [Colors.blue, Colors.green],
           ),
         ),
         child: Column(
           children: [
+            Flexible(flex: 1, child: Container()),
             Flexible(
+              flex: 5,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(DateFormat('HH${showColon ? ':' : ' '}mm').format(now)),
-                  Text(DateFormat("dd/MM/yyyy").format(now)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        DateFormat('HH').format(now),
+                        style: tStyle,
+                      ),
+                      AnimatedOpacity(
+                        opacity: showColon ? 1 : 0,
+                        duration: const Duration(milliseconds: 100),
+                        child: Text(
+                          ':',
+                          style: tStyle,
+                        ),
+                      ),
+                      Text(
+                        DateFormat('mm').format(now),
+                        style: tStyle,
+                      ),
+                    ],
+                  ),
+                  Text(
+                    DateFormat('dd/MM/yyyy').format(now),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontStyle: FontStyle.italic,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      shadows: <Shadow>[
+                        Shadow(
+                          offset: Offset(0.5, 0.5),
+                          blurRadius: 1.0,
+                          color: Colors.black,
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
             Flexible(
+              flex: 5,
               child: Center(
                 child: RaisedButton(
-                  child: Text('BEGIN WORK'),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: Text(
+                      'START WORKING 💪',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
                   onPressed: () {},
+                  color: Colors.indigo[400],
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(6),
+                  ),
                 ),
               ),
             ),
+            Flexible(flex: 1, child: Container()),
           ],
         ),
       ),
