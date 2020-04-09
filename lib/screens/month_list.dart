3d70f0fedaf4de24d94b18e12691cb8869c76da7
Augustin <augustin@mycompanyfiles.fr>
2020-04-09 11:51:53 +0200

    WIP on add charts

--- lib/screens/month_list.dart
+++ lib/screens/month_list.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
@@ -15,6 +16,108 @@ class MontListPage extends StatelessWidget {
   }) : super(key: key);
 
   final String month;
+
+  LineChartData mainData(Map<String, List<Rush>> data) {
+    final now = DateTime.now();
+    double maxX =
+        now.difference(DateTime(now.year, now.month)).inDays.toDouble();
+    double maxY = 0;
+    List<FlSpot> spots = [];
+    for (int i = 1; i <= maxX + 1; i++) {
+      final key = data.keys.firstWhere(
+          (key) => int.parse(key.split('/')[0]) == i,
+          orElse: () => null);
+      if (key == null) {
+        final spot = FlSpot(i.toDouble(), 0);
+        spots.add(spot);
+      } else {
+        Duration total = Duration.zero;
+        for (final rush in data[key]) {
+          final duration = rush.endDate.difference(rush.startDate);
+          total += duration;
+        }
+        if (total.inHours > maxY) maxY = total.inHours.toDouble();
+        final spot = FlSpot(
+          i.toDouble(),
+          total.inHours.toDouble(),
+        );
+        spots.add(spot);
+      }
+    }
+    return LineChartData(
+      gridData: FlGridData(
+        show: true,
+        drawVerticalLine: true,
+        getDrawingHorizontalLine: (value) {
+          return const FlLine(
+            color: Color(0xff37434d),
+            strokeWidth: 1,
+          );
+        },
+        getDrawingVerticalLine: (value) {
+          return const FlLine(
+            color: Color(0xff37434d),
+            strokeWidth: 1,
+          );
+        },
+      ),
+      titlesData: FlTitlesData(
+        show: true,
+        bottomTitles: SideTitles(
+          showTitles: true,
+          reservedSize: 22,
+          textStyle: const TextStyle(
+            color: Color(0xff68737d),
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
+          getTitles: (value) => '${value.toInt()}',
+          margin: 8,
+        ),
+        leftTitles: SideTitles(
+          showTitles: true,
+          textStyle: const TextStyle(
+            color: Color(0xff67727d),
+            fontWeight: FontWeight.bold,
+            fontSize: 15,
+          ),
+          getTitles: (value) => '${value.toInt()}',
+          reservedSize: 28,
+          margin: 12,
+        ),
+      ),
+      borderData: FlBorderData(
+        show: true,
+        border: Border.all(color: const Color(0xff37434d), width: 1),
+      ),
+      minX: 1,
+      maxX: maxX + 1,
+      minY: 0,
+      maxY: maxY,
+      lineBarsData: [
+        LineChartBarData(
+          spots: spots,
+          isCurved: true,
+          colors: gradientColors,
+          barWidth: 5,
+          isStrokeCapRound: true,
+          dotData: const FlDotData(
+            show: false,
+          ),
+          belowBarData: BarAreaData(
+            show: true,
+            colors:
+                gradientColors.map((color) => color.withOpacity(0.3)).toList(),
+          ),
+        ),
+      ],
+    );
+  }
+
+  static const List<Color> gradientColors = [
+    const Color(0xff23b6e6),
+    const Color(0xff02d39a),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -53,6 +156,21 @@ class MontListPage extends StatelessWidget {
                       child: Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: Text(month),
+                      ),
+                    ),
+                  ),
+                  AspectRatio(
+                    aspectRatio: 1.70,
+                    child: Container(
+                      decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(18),
+                          ),
+                          color: Color(0xff232d37)),
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            right: 18.0, left: 12.0, top: 24, bottom: 12),
+                        child: LineChart(mainData(dSortedRushes)),
                       ),
                     ),
                   ),
